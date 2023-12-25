3f1161fc34fbf66bd5a26852ce7a201caadf5e39
baicany <110486186+baicany@users.noreply.github.com>
2023-12-25 18:23:08 +0800

    Delete web/ai_java directory

--- web/ai_java/flag.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "40c1273690a4c707335d95877b943054e9f9d391" | /usr/games/cowsay
--- web/ai_java/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 启动 MySQL 服务
-service mysql start
-
-
-
-# 运行 Java 应用程序
-/java/bin/java -jar /ctf-0.0.1-SNAPSHOT.jar
